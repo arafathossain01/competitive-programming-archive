d77ad7a626efa1f07c66f003a817cb1b6b802e16
Arafat Hossain <arafathossain.cs@gmail.com>
2026-01-06 00:35:35 +0600

    template

---
+++ CP_template/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//short form of datatypes
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+//macros
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+int main()
+{
+    ll a = 1000000;
+    cout<<a<<endl;
+    return 0;
+}
